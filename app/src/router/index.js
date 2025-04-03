66db0458caabebf2eecb8dba8ba5f79e3f41a88b
Daniel Alba <danielorlandoa8@gmail.com>
2025-04-03 02:20:21 -0500

    feat(app): create routes

---
+++ app/src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '@/views/Login.vue';
+import Dashboard from '@/views/Dashboard.vue';
+import { useAuthStore } from '@/stores/auth';
+import DetailVm from '@/views/DetailVm.vue';
+
+const routes = [
+    { path: '/login', component: Login },
+    {
+        path: '/dashboard',
+        component: Dashboard,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/vm/:id',
+        component: DetailVm,
+        meta: { requiresAuth: true }
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore();
+
+    if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+        next('/login');
+    } else {
+        next();
+    }
+});
+
+export default router;
