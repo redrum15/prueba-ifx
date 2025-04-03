91db9106c7e98c99745dcf8a55a13bfb5d8a8015
Daniel Alba <danielorlandoa8@gmail.com>
2025-04-03 05:34:02 -0500

    feat(api): create handler for success response

--- api/src/handlers/http.go
+++ api/src/handlers/http.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 )
 
 func SendJSONError(w http.ResponseWriter, message string, statusCode int) {
@@ -11,4 +13,18 @@ func SendJSONError(w http.ResponseWriter, message string, statusCode int) {
 
 	errorResponse := map[string]string{"error": message}
 	json.NewEncoder(w).Encode(errorResponse)
+}
+
+func SendJSONSuccess(w http.ResponseWriter, response interface{}, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if response != nil {
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			log.Error().Err(err).Msg("Error encoding response")
+			http.Error(w, "Error generating response", http.StatusInternalServerError)
+			return
+		}
+	}
+
 }
