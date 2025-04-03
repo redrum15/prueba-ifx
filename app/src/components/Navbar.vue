51019f303163d0046c543a6ae21c2ac36fc26c53
Daniel Alba <danielorlandoa8@gmail.com>
2025-04-03 02:19:45 -0500

    feat(app): create navbar component

---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+import { useRouter } from 'vue-router';
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+const logout = () => {
+    authStore.logout();
+    router.push('/login');
+};
+
+</script>
+
+<template>
+    <nav class="navbar bg-body-tertiary">
+        <form class="container-fluid justify-content-between">
+            <p>Prueba IFX</p>
+            <div>
+                <button v-if="authStore.user.user_type === 'admin'" class="btn btn-outline-success me-2" type="button"
+                    data-bs-toggle="modal" data-bs-target="#createVMModal">
+                    Create VM</button>
+                <button @click="logout" class="btn btn-outline-secondary" type="button">Logout</button>
+            </div>
+        </form>
+    </nav>
+</template>
