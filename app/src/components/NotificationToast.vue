5e707f82628a37ff733e671e9ce60e435e45f8f6
Daniel Alba <danielorlandoa8@gmail.com>
2025-04-03 06:04:23 -0500

    style(app): make bg color variable

--- app/src/components/NotificationToast.vue
+++ app/src/components/NotificationToast.vue
@@ -5,6 +5,13 @@ import { computed, onMounted } from "vue";
 const websocketStore = useWebSocketStore();
 const lastMessage = computed(() => websocketStore.messages.at(-1));
 
+const toastClass = computed(() => {
+    return {
+        "bg-success": lastMessage.value?.event_type === "created",
+        "bg-danger": lastMessage.value?.event_type === "deleted",
+        "bg-warning": lastMessage.value?.event_type === "updated"
+    };
+});
 
 onMounted(() => {
     websocketStore.connect();
@@ -13,15 +20,13 @@ onMounted(() => {
 
 <template>
     <div v-if="lastMessage" class="toast-container position-fixed top-0 end-0 p-3">
-        <div class="toast show bg-info text-white" role="alert">
-            <div class="toast-header">
-                <strong class="me-auto">Notificación</strong>
-                <button type="button" class="btn-close" @click="websocketStore.messages.pop()"
-                    aria-label="Close"></button>
+        <div id="liveToast" class="toast d-flex" :class="toastClass" role="alert" ref="toastRef" aria-live="assertive"
+            aria-atomic="true">
+            <div class="toast-body text-white fw-bolder">
+                {{ lastMessage.message }}
             </div>
-            <div class="toast-body">
-                {{ lastMessage.text }}
-            </div>
+            <button @type="button" class="btn-close me-2 m-auto" @click="websocketStore.messages.pop()"
+                aria-label="Close"></button>
         </div>
     </div>
 </template>
