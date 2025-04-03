c2421c7230cabe0ed7554e62a9fe510fda167e68
Daniel Alba <danielorlandoa8@gmail.com>
2025-04-03 02:17:13 -0500

    feat(api): create sockets file

---
+++ api/src/handlers/sockets.go
@@ -0,0 +1,65 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type VMEvent struct {
+	EventType string `json:"event_type"`
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var clientsMutex = sync.Mutex{}
+
+var broadcast = make(chan VMEvent)
+
+func GetBroadcast() chan VMEvent {
+	return broadcast
+}
+
+func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	clientsMutex.Lock()
+	clients[conn] = true
+	clientsMutex.Unlock()
+
+	for {
+		_, _, err := conn.ReadMessage()
+		if err != nil {
+			clientsMutex.Lock()
+			delete(clients, conn)
+			clientsMutex.Unlock()
+			break
+		}
+	}
+}
+
+func NotifyClients(vm VMEvent) {
+	clientsMutex.Lock()
+	defer clientsMutex.Unlock()
+
+	message, _ := json.Marshal(vm)
+
+	for client := range clients {
+		err := client.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			client.Close()
+			delete(clients, client)
+		}
+	}
+}
