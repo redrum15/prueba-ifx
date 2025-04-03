01c9da6ec217480cde4894673f7472496e954144
Daniel Alba <danielorlandoa8@gmail.com>
2025-04-03 02:19:03 -0500

    feat(app): create dashboard view

---
+++ app/src/views/Dashboard.vue
@@ -0,0 +1,130 @@
+<script setup>
+import { handlerRequest } from "@/rest/index";
+import { useAuthStore } from '@/stores/auth';
+import { useRouter } from 'vue-router';
+import { onMounted, ref, computed } from 'vue';
+import { formatDistanceToNow, parseISO } from "date-fns";
+import { API_URLS } from "@/rest/urls";
+import Navbar from '@/components/Navbar.vue';
+
+const router = useRouter();
+
+const toastRef = ref(null);
+const toastMessage = ref("test");
+const showToastFlag = ref(false);
+const authStore = useAuthStore();
+
+
+const items = ref([]);
+const formData = ref({
+    name: "",
+    cores: "",
+    ram: "",
+    os: "",
+    status: ""
+});
+
+const formattedItems = computed(() =>
+    items.value.map(item => ({
+        ...item,
+        create_at: formatDistanceToNow(parseISO(item.CreatedAt), { addSuffix: true })
+    }))
+);
+
+
+const handleVMCreate = async () => {
+    try {
+        await handlerRequest("POST", API_URLS.VMS.DEFAULT, formData.value);
+        toastMessage.value = "VM created successfully";
+    } catch (error) {
+        toastMessage.value = error.message;
+    } finally {
+        showToastFlag.value = true;
+        showToast();
+    }
+};
+
+
+
+onMounted(async () => {
+    try {
+        items.value = await handlerRequest("GET", API_URLS.VMS.DEFAULT);
+    } catch (error) {
+        toastMessage.value = error.message;
+        showToastFlag.value = true;
+        showToast();
+    }
+});
+
+
+const goToDetail = (id) => {
+    router.push(`/vm/${id}`);
+};
+</script>
+
+<template>
+    <Navbar />
+    <p class="p-2">User type: {{ authStore.user.user_type }}</p>
+    <h1 class="mt-4">Virtual Machines:</h1>
+    <div class="toast-container position-fixed top-0 end-0 p-3" :hidden="!showToastFlag">
+        <div id="liveToast" class="toast d-flex bg-danger " role="alert" ref="toastRef" aria-live="assertive"
+            aria-atomic="true">
+            <div class="toast-body text-white fw-bolder">
+                {{ toastMessage }}
+            </div>
+            <button @click="showToastFlag = false" type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast"
+                aria-label="Close"></button>
+        </div>
+    </div>
+
+    <div class="modal fade" id="createVMModal" tabindex="-1" aria-labelledby="createVMModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form @submit.prevent="handleVMCreate" class="p-3">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input v-model="formData.name" type="text" class="form-control" id="name"
+                            aria-describedby="nameHelp" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="cores" class="form-label">Cores</label>
+                        <input v-model="formData.cores" type="number" class="form-control" id="cores"
+                            aria-describedby="coresHelp" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="ram" class="form-label">Ram</label>
+                        <input v-model="formData.ram" type="number" class="form-control" id="ram"
+                            aria-describedby="ramHelp" required>
+                    </div>
+
+
+                    <div class="mb-3">
+                        <label for="os" class="form-label">OS</label>
+                        <input v-model="formData.os" type="text" class="form-control" id="os" aria-describedby="osHelp"
+                            required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="status" class="form-label">Status</label>
+                        <input v-model="formData.status" type="text" class="form-control" id="status"
+                            aria-describedby="statusHelp" required>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="list-group mt-1">
+        <a v-for="item in formattedItems" :key="item.ID" href="#" class="list-group-item list-group-item-action"
+            aria-current="true" @click="goToDetail(item.ID)">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">{{ item.Name }}</h5>
+                <small>{{ item.create_at }}</small>
+            </div>
+            <p class="mb-1">{{ item.Cores }}</p>
+            <small>{{ item.Status }}</small>
+        </a>
+    </div>
+</template>
