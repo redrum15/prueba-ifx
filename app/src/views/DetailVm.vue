d3da526037012f87cf7230d31012f738e48049f0
Daniel Alba <danielorlandoa8@gmail.com>
2025-04-03 06:09:45 -0500

    feat(app): add edit form

--- app/src/views/DetailVm.vue
+++ app/src/views/DetailVm.vue
@@ -12,25 +12,30 @@ const route = useRoute();
 const router = useRouter();
 const authStore = useAuthStore();
 
-const data = ref("")
+const data = ref(null);
+const isEditing = ref(false);
 const modalRef = ref(null);
 let modalInstance = null;
-
 
 const websocketStore = useWebSocketStore();
 const lastMessage = computed(() => websocketStore.messages.at(-1));
 
-
-
 const handleVMDelete = async () => {
     await handlerRequest("DELETE", API_URLS.VMS.DELETE(route.params.id));
     goBack();
-}
+};
 
+const startEditing = () => {
+    isEditing.value = true;
+};
+
+const saveChanges = async () => {
+    await handlerRequest("PUT", API_URLS.VMS.EDIT(route.params.id), data.value);
+    isEditing.value = false;
+};
 
 onMounted(async () => {
     modalInstance = new Modal(modalRef.value);
-
     data.value = await handlerRequest("GET", API_URLS.VMS.DETAIL(route.params.id));
 });
 
@@ -44,27 +49,51 @@ onBeforeRouteLeave((to, from, next) => {
     }
     next();
 });
-
-
 </script>
+
 <template>
     <Navbar />
 
     <button @click="goBack()" class="btn btn-link">&lt; back</button>
     <div class="container d-flex justify-content-between mt-4">
         <div>
-            <p>Name: {{ data.name }}</p>
-            <p>Cores: {{ data.cores }}</p>
-            <p>Ram: {{ data.ram }}</p>
-            <p>OS: {{ data.os }}</p>
-            <p>Status: {{ data.status }}</p>
+            <template v-if="!isEditing">
+                <p>Name: {{ data.name }}</p>
+                <p>Cores: {{ data.cores }}</p>
+                <p>Ram: {{ data.ram }}</p>
+                <p>OS: {{ data.os }}</p>
+                <p>Status: {{ data.status }}</p>
+            </template>
+            <template v-else>
+                <div class="mb-2">
+                    <label class="form-label">Name</label>
+                    <input v-model="data.name" type="text" class="form-control">
+                </div>
+                <div class="mb-2">
+                    <label class="form-label">Cores</label>
+                    <input v-model="data.cores" type="number" class="form-control">
+                </div>
+                <div class="mb-2">
+                    <label class="form-label">Ram</label>
+                    <input v-model="data.ram" type="number" class="form-control">
+                </div>
+                <div class="mb-2">
+                    <label class="form-label">OS</label>
+                    <input v-model="data.os" type="text" class="form-control">
+                </div>
+                <div class="mb-2">
+                    <label class="form-label">Status</label>
+                    <input v-model="data.status" type="text" class="form-control">
+                </div>
+            </template>
         </div>
+
         <div v-if="authStore.user.user_type === 'admin'">
-            <button @click="goBack()" class="btn btn-warning me-2">EDIT</button>
+            <button v-if="!isEditing" @click="startEditing" class="btn btn-warning me-2">EDIT</button>
+            <button v-if="isEditing" @click="saveChanges" class="btn btn-success me-2">SAVE</button>
             <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteVMModal">DELETE</button>
         </div>
     </div>
-
 
     <div class="modal" tabindex="-1" id="deleteVMModal" aria-labelledby="deleteVMModal" aria-hidden="true"
         ref="modalRef">
