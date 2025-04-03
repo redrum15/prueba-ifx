d73ae1eb8b72603dcb78278be3376f396e4a16d6
Daniel Alba <danielorlandoa8@gmail.com>
2025-04-03 02:21:27 -0500

    feat(app): create app.vue file

---
+++ app/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { onMounted } from "vue";
+import { useWebSocketStore } from "@/stores/socket";
+import NotificationToast from "@/components/NotificationToast.vue";
+
+
+
+</script>
+
+<template>
+  <router-view />
+  <NotificationToast />
+
+</template>
